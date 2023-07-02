fc7812d4caf3c4427beceb02f6c4105a754dd9b7
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-07-01 21:33:11 -0300

    feat: cria o componente concreto do nome da musica

---
+++ design-patterns-project/DPAvalII2023.1/src/br/edu/ifba/inf011/model/decorator/ConcreteComponentNomeMusica.java
@@ -0,0 +1,41 @@
+package br.edu.ifba.inf011.model.decorator;
+
+import br.edu.ifba.inf011.model.Musica;
+
+// Componente Concreto no pattern Decorator
+public class ConcreteComponentMusica implements Musica {
+
+    private final String nome;
+    private Integer linha;
+
+    public ConcreteComponentMusica(String nome){
+        this.nome = nome;
+        this.reset();
+    }
+    @Override
+    public String getNome() {
+        return null;
+    }
+    @Override
+    public String execute() {
+        return " ";
+    }
+    @Override
+    public void reset() {
+        this.linha = 0;
+    }
+
+    @Override
+    public Boolean finish() {
+        return this.linha>0;
+    }
+
+    @Override
+    public String play() {
+        if (!this.finish()){
+            this.linha++;
+            return "\n---------:  \t\t" +this.getNome() + "\n";
+        }
+        return this.execute();
+    }
+}
