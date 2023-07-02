542dbdad1d29798e6ace16f2c1562430219355c9
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-07-02 02:42:47 -0300

    feat: implementação dao metodo de randomize. este seleciona um item pseudo aleatorio da playlist e o executa

--- design-patterns-project/DPAvalII2023.1/src/br/edu/ifba/inf011/model/Playlist.java
+++ design-patterns-project/DPAvalII2023.1/src/br/edu/ifba/inf011/model/Playlist.java
@@ -4,25 +4,26 @@ import br.edu.ifba.inf011.model.composite.ComponentPlaylistItem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Playlist implements ComponentPlaylistItem {
-	
+
 	private final String nome;
 	private List<ComponentPlaylistItem> componenteItems;
 //	private List<Playlist> musicas;
 //	private List<Playlist> playlists;
-	
+
 	public Playlist(String nome) {
 		this.nome = nome;
 		this.componenteItems = new ArrayList<>();
 //		this.musicas = new ArrayList<Musica>();
 //		this.playlists = new ArrayList<Playlist>();
 	}
-	
+
 	public void insert(ComponentPlaylistItem componenteItem) {
 		this.componenteItems.add(componenteItem);
 	}
-	
+
 	public void remove(ComponentPlaylistItem componentPlaylistItem) {
 		this.componenteItems.remove(componentPlaylistItem);
 	}
@@ -30,13 +31,23 @@ public class Playlist implements ComponentPlaylistItem {
 	public String getNome() {
 		return this.nome;
 	};
-	
+
 	public String randomize() {
+		if (!componenteItems.isEmpty()){
+			Random random = new Random();
+			int sorteadado = random.nextInt(componenteItems.size());
+			ComponentPlaylistItem componentItemAleatorio = componenteItems.get(sorteadado);
+			if (componentItemAleatorio instanceof  Musica){
+				return componentItemAleatorio.execute();
+			}
+			Playlist playlistRandom = (Playlist) componentItemAleatorio;
+			return playlistRandom.randomize();
+		}
 		return null;
 	};
-	
+
 	public String execute() {
 		return null;
-	}	
+	}
 
 }
